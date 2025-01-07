e3bebf85ec19c7aa1dd5d846c3475404832269e5
Yash Thaker <58665521+yashthr@users.noreply.github.com>
2025-01-07 12:04:18 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "app_name" {
+  description = "Application name"
+  type        = string
+  default     = "devops-demo"
+}
